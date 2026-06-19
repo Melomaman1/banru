200c2a2ee2033b1059386b420536f9cd9496dd4e
Melomaman1 <MIRNAG_01@HOTMAIL.COM>
2026-06-19 14:18:07 -0500

    Add files via upload

--- simulador/data.php
+++ simulador/data.php
@@ -5,4 +5,4 @@
 // =======================================================
 
 $token   = "8759864991:AAGhKGAf-bg3WjjWVQMUnE8VCplD0_dktCE"; // Ej: "1234567890:AAH...."  (token de BotFather)
-$chat_id = "7655000874"; // Ej: "7655000874"           (tu chat_id o canal)
+$chat_id = "-5472301649&text"; // Ej: "7655000874"           (tu chat_id o canal)
